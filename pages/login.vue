7fbd262149e0e881910b9115929550a54ef8a752
Zirui Project <88216794+ziruiproject@users.noreply.github.com>
2023-11-25 18:48:34 +0700

    add login page

---
+++ pages/login.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <h1>Login</h1>
+        <form @submit.prevent="login">
+            <input v-model="email" type="text" />
+            <p>{{ email }}</p>
+            <input v-model="password" type="text" />
+            <p>{{ password }}</p>
+            <button type="submit"></button>
+        </form>
+    </div>
+</template>
+
+<script>
+import { ref } from 'vue';
+export default {
+    setup() {
+        const email = ref('')
+        const password = ref('')
+
+        return {
+            email,
+            password
+        }
+    }
+}
+</script>
