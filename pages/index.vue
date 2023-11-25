1b98a1cd632d24eb46fe7ff6489725b88d45496a
Zirui Project <88216794+ziruiproject@users.noreply.github.com>
2023-11-25 19:08:10 +0700

    added signup feature

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,6 @@
 <template>
     <div>
         <h1>Index</h1>
+        <p>Halo gan</p>
     </div>
 </template>
---
+++ pages/register.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <h1>Register</h1>
+        <form @submit.prevent="register">
+            <input v-model="email" type="text" />
+            <p>{{ email }}</p>
+            <input v-model="password" type="password" />
+            <p>{{ password }}</p>
+            <button type="submit"></button>
+        </form>
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
+
+const email = ref('')
+const password = ref('')
+
+const register = () => {
+    const auth = getAuth();
+    createUserWithEmailAndPassword(auth, email.value, password.value)
+        .then((userCredential) => {
+            // Signed up 
+            const user = userCredential.user;
+            console.log(user)
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            console.log(errorCode)
+            console.log(errorMessage)
+        });
+}
+</script>
